54060e131520d4c96d96ef9dba6e34899412dc80
esbnet <esbnet@gmail.com>
2023-11-20 22:21:46 -0300

    feat:

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,20 +1,8 @@
-import { StyleSheet, Text } from 'react-native';
+import { Container } from "./styles";
 
-import { Container } from './styles';
-
-export default function Groups() {
+export function Groups() {
   return (
     <Container>
-      <Text style={styles.text}>
-        Groups
-        </Text>
     </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  text: {
-    color: '#fff',
-    fontSize: 32
-  }
-});
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,8 +1,7 @@
-import styled from "styled-components/native";
+import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  backgroundColor: #000;
-  alignItems: center;
-  justifyContent: center;
-`
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+  padding: 24px;
+`;
