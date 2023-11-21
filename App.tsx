128e1b0b5a8bf94c201f31ab85162786ac688669
esbnet <esbnet@gmail.com>
2023-11-21 15:24:16 -0300

    feat: :sparkles: created loading fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
-import { Groups } from '@/screens/Groups';
-import { ThemeProvider } from 'styled-components/native';
-import theme from './src/theme';
+import { ThemeProvider } from "styled-components/native";
+
+import { Groups } from "@/screens/Groups";
+
+import theme from "./src/theme";
+
+import { Loading } from "@/components/Loading";
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  })
+
+  
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      { fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
