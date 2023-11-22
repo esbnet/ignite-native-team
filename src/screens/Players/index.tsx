d5fc42fd6d5f777c3750f0f00fcad0771a303f37
esbnet <esbnet@gmail.com>
2023-11-22 18:17:54 -0300

    feat: :sparkles: created player component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,11 +3,14 @@ import { Filter } from "@/components/Filter";
 import { Header } from "@/components/Header";
 import { Highlight } from "@/components/Highlight";
 import { Input } from "@/components/Input";
+import { PlayerCard } from "@/components/PlayerCard";
 import { useState } from "react";
-import { Container, Form } from "./styles";
+import { FlatList } from "react-native";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 export function Players() {
-  const [groups, setGroups] = useState([]);
+  const [team, setTeam] = useState("Time A");
+  const [players, setPlayers] = useState(["Edmilson", "Cleide"]);
 
   return (
     <Container>
@@ -22,9 +25,29 @@ export function Players() {
         <ButtonIcon type="PRIMARY" icon="add" />
       </Form>
 
-      <Filter title="Time A" isActive />
-      <Filter title="Time B"  />
-      
+      <HeaderList>
+        <FlatList
+          data={["Time A", "Time B"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+      />
     </Container>
   );
 }
